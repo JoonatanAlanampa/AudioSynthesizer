8343d63d9b63e24d7af7fd0cee004f55f83513a3
Joonatan Alanampa <joonatan.alanampa1@gmail.com>
2025-11-08 12:39:10 +0200

    Update audio_ctrl.vhd

--- Synthesizer/audio_ctrl.vhd
+++ Synthesizer/audio_ctrl.vhd
@@ -1,103 +1,127 @@
 library ieee;
 use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
 
-entity audio_codec_model is
-    generic (
-        data_width_g : integer := 16
-    );
-    port (
-        rst_n           : in std_logic;
-        aud_data_in     : in std_logic;
-        aud_bclk_in     : in std_logic;
-        aud_lrclk_in    : in std_logic;
-        value_left_out  : out std_logic_vector(data_width_g-1 downto 0);
-        value_right_out : out std_logic_vector(data_width_g-1 downto 0)
-    );
-end audio_codec_model;
+entity audio_ctrl is
+  generic (
+    ref_clk_freq_g : integer := 12288000;
+    sample_rate_g  : integer := 48000;
+    data_width_g   : integer := 16
+  );
+  port (
+    clk           : in  std_logic;
+    rst_n         : in  std_logic;
+    left_data_in  : in  std_logic_vector(data_width_g-1 downto 0);
+    right_data_in : in  std_logic_vector(data_width_g-1 downto 0);
+    aud_bclk_out  : out std_logic;
+    aud_data_out  : out std_logic;
+    aud_lrclk_out : out std_logic
+  );
+end audio_ctrl;
 
-architecture rtl of audio_codec_model is
+architecture rtl of audio_ctrl is
+	
+	constant bclk_max_c  : integer := ref_clk_freq_g / (2*2*data_width_g*sample_rate_g);
 
-    type states_type is (wait_for_input, read_left, read_right);
-    signal current_state_r : states_type;
-    signal left_result_r   : std_logic_vector(data_width_g-1 downto 0);
-    signal right_result_r  : std_logic_vector(data_width_g-1 downto 0);
-    signal shift_r         : std_logic_vector(data_width_g-1 downto 0);
-    signal bit_cnt_r       : integer range 0 to data_width_g-1;
-    signal lrclk_prev_r    : std_logic;
-
+	signal right_data_r  : std_logic_vector(data_width_g-1 downto 0);
+	signal shift_r       : std_logic_vector(data_width_g-1 downto 0);
+	signal data_out_r    : std_logic;
+	signal bit_cnt_r     : integer range 0 to data_width_g-1;
+	
+	signal bclk_r        : std_logic;
+	signal bclk_cnt_r    : integer range 0 to bclk_max_c-1;
+	signal bclk_fall_r   : std_logic;
+	
+	signal lrclk_r       : std_logic;
+	signal sync_r        : std_logic;
 
 begin
 
-    value_left_out  <= left_result_r;
-    value_right_out <= right_result_r;
+	aud_data_out <= data_out_r;
+	aud_bclk_out <= bclk_r;
+	aud_lrclk_out <= lrclk_r;
+	
+  process (clk, rst_n)
+    begin
 
-    process (aud_bclk_in, rst_n)
-        begin
-            if (rst_n = '0') then
-                -- Init state (wait_for_input) and init output of FSM
-                current_state_r <= wait_for_input;
-                left_result_r <= (others => '0');
-                right_result_r <= (others => '0');
-                shift_r <= (others => '0');
-                bit_cnt_r <= 0;
-                lrclk_prev_r <= '0';
+      -- Initialization on reset
 
-            -- The MSB of right and left channel are valid on the rising edge of the bit clock
+      if (rst_n = '0') then
+        right_data_r  <= (others => '0');
+        shift_r       <= (others => '0');
+        data_out_r    <= '0';
+        bit_cnt_r     <= 0;
+        bclk_r        <= '0';
+        bclk_cnt_r    <= 0;
+        bclk_fall_r   <= '0';
+        lrclk_r       <= '0';
+        sync_r        <= '0';
 
-            elsif (aud_bclk_in'event and aud_bclk_in = '1') then
-                
-                -- The MSB of the left channel is valid on the rising edge of the bit clock
-                -- following the rising edge of the word clock
-                -- put the MSB into shift register
+      elsif (clk'event and clk = '1') then
 
-                if (aud_lrclk_in = '1' and lrclk_prev_r = '0') then
-                    current_state_r <= read_left;
-                    shift_r <= shift_r(data_width_g-2 downto 0) & aud_data_in;
-                    bit_cnt_r <= 1;
+        -- Reset bclk falling edge flag
 
-                -- The MSB of the right channel is valid on the rising edge of the bit clock
-                -- following the falling edge of the word clock
-                -- read_right state can only be entered from read_left state so not from wait_for_input
-                -- put the MSB into shift register
+        bclk_fall_r  <= '0';
 
-                elsif (aud_lrclk_in = '0' and lrclk_prev_r = '1' and current_state_r = read_left) then
-                    current_state_r <= read_right;
-                    shift_r <= shift_r(data_width_g-2 downto 0) & aud_data_in;
-                    bit_cnt_r <= 1;
+        -- Generate bclk
 
-                -- No word clock edges present so stay in current state
+        if (bclk_cnt_r = bclk_max_c-1) then
+          bclk_cnt_r <= 0;
+          if(bclk_r = '1') then
+            bclk_fall_r <= '1';
+          end if;
+          bclk_r <= not bclk_r;
+        else
+          bclk_cnt_r <= bclk_cnt_r+1;
+        end if;
 
-                else
+        -- Sync bclk and lrclk
+        -- Snapshot data inputs
+        -- First data transition begins left channel first, then right
 
-                    -- read_left state updates left output
-                    
-                    if (current_state_r = read_left) then
-                        -- When the whole word is put into shift register, update left output of codec model
-                        if (bit_cnt_r = data_width_g-1) then
-                            left_result_r <= shift_r(data_width_g-2 downto 0) & aud_data_in;
-                            bit_cnt_r <= 0;
-                        else
-                            shift_r <= shift_r(data_width_g-2 downto 0) & aud_data_in;
-                            bit_cnt_r <= bit_cnt_r+1;
-                        end if;
-                    
-                    -- read_right state updates right output
+        if (sync_r = '0' and bclk_fall_r = '1') then
+          lrclk_r <= '1';
+          sync_r <= '1';
+          bit_cnt_r <= 0;
+          right_data_r <= right_data_in;
+          shift_r <= left_data_in;
+          data_out_r <= left_data_in(data_width_g-1);
 
-                    elsif (current_state_r = read_right) then
-                        -- When the whole word is put into shift register, update right output of codec model
-                        if (bit_cnt_r = data_width_g-1) then
-                            right_result_r <= shift_r(data_width_g-2 downto 0) & aud_data_in;
-                            bit_cnt_r <= 0;
-                        else
-                            shift_r <= shift_r(data_width_g-2 downto 0) & aud_data_in;
-                            bit_cnt_r <= bit_cnt_r+1;
-                        end if;
-                    end if;
-                end if;
+        -- Generate lrclk and send the bits from input through shift register to output
 
-            lrclk_prev_r <= aud_lrclk_in;
+        elsif (sync_r = '1' and bclk_fall_r = '1') then
 
+          -- 16 bits sent, switch channel
+
+          if (bit_cnt_r = data_width_g-1) then
+
+            -- On lrclk logic level 0 send bits from right channel
+
+            if (lrclk_r = '1') then
+              lrclk_r <= '0';
+              bit_cnt_r <= 0;
+              shift_r <= right_data_r;
+              data_out_r <= right_data_r(data_width_g-1);
+
+            -- On lrclk logic level 1 send bits from left channel
+
+            elsif (lrclk_r = '0') then
+              lrclk_r <= '1';
+              bit_cnt_r <= 0;
+              right_data_r <= right_data_in;
+              shift_r <= left_data_in;
+              data_out_r <= left_data_in(data_width_g-1);
             end if;
-    end process;
+          
+          -- Send bits from MSB to LSB (MSB is sent during channel transition already)
+          
+          else
+            data_out_r <= shift_r(data_width_g-2);
+            shift_r <= shift_r(data_width_g-2 downto 0) & '0';
+            bit_cnt_r <= bit_cnt_r+1;
+          end if;
+        end if;
+      end if;
+  end process;
 
 end rtl;
